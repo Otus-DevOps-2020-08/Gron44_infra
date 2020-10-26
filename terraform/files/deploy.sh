0b8174d4bedda570e881ce9f58f062cb2b29edab
Ilya Fedorenko <fargal2@yandex.ru>
2020-10-26 22:45:29 +0300

    ДЗ № 10 Ansible-1 *

--- terraform/files/deploy.sh
+++ terraform/files/deploy.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 set -e
 APP_DIR=${1:-$HOME}
+
+#check for locked files
+lock_files=(
+    /var/lib/dpkg/lock
+    /var/lib/apt/lists/lock
+    /var/lib/dpkg/lock-frontend
+    /var/cache/apt/archives/lock
+    /var/lib/apt/daily_lock
+)
+for file in "${lock_files[@]}"; do
+    while fuser "$file" >/dev/null 2>&1; do
+        echo "Waiting for lock $file to be available..."
+        sleep 3
+    done
+done
+
 if [ $(dpkg-query -W -f='${Status}' git 2>/dev/null | grep -c "ok installed") -eq 0 ]
     then
         sudo apt update -y
-        sleep 5
         sudo apt install git -y
 else
     echo "git already installed.  Skipping..."
